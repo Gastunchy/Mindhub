357c17420377d81f32aa6aebe1a5eb29bb88d91a
Gastunchy <97688245+Gastunchy@users.noreply.github.com>
2024-02-16 16:22:44 -0300

    Update eventosAction.js

--- src/store/actions/eventosAction.js
+++ src/store/actions/eventosAction.js
@@ -4,7 +4,7 @@ import axios from "axios";
 const get_eventos = createAsyncThunk("get_eventos", async () => {
     try {
         let eventos = await axios
-            .get("http://localhost:3000/api/eventos/")
+            .get("http://backend-api:3000/api/eventos/")
             .then((response) => {
 
                 return response.data.eventos;
