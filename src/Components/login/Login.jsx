ffff600951d7ae30279db51a89fca9704d4055ce
Gastunchy <97688245+Gastunchy@users.noreply.github.com>
2024-02-17 08:38:37 -0300

    Update Login.jsx

--- src/Components/login/Login.jsx
+++ src/Components/login/Login.jsx
@@ -14,7 +14,7 @@ const Login = () => {
     
     const handleSignIn = () => {
 
-        axios.post("http://backend-api:3000/api/login/", {
+        axios.post("http://34.16.191.24:3000/api/login/", {
             email: emailInputRef.current.value,
             password: passwordInputRef.current.value,
         })
