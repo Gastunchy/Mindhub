685c98185abb5cea35028a2b9352ce1bbc8fac81
Gastunchy <97688245+Gastunchy@users.noreply.github.com>
2024-03-06 08:50:46 -0300

    Update eventosAction.js

--- src/store/actions/eventosAction.js
+++ src/store/actions/eventosAction.js
@@ -4,7 +4,7 @@ import axios from "axios";
 const get_eventos = createAsyncThunk("get_eventos", async () => {
     try {
         let eventos = await axios
-            .get("http://back.34.73.198.248.nip.io/api/eventos/")
+            .get("http://localhost:3000/api/eventos/")
             .then((response) => {
 
                 return response.data.eventos;
